6904d0b1d0def5c523b36a64c3d3d79f2af60712
Ocalak <96614838+Ocalak@users.noreply.github.com>
2024-05-30 21:59:41 +0200

    Update holidays.R

--- holidays.R
+++ holidays.R
@@ -1,53 +1,75 @@
-#
-holiday_dates <- holidays_2000_2050
-holiday_dates$Date <- as.POSIXct(holiday_dates$Date)
+# Load necessary libraries
+library(lubridate)
+library(dplyr)
+holidays_2000_2050
+holidays <- holidays_2000_2050 %>% filter(CountryCode == "ES")
+fixed <- holidays %>% filter(Fixed == TRUE & Global == TRUE)
+#6#24#6
+assdays <- holidays %>% filter(Name=="Assumption")
+fixed <- rbind(fixed,assdays)%>% arrange(Date)
 
-holiday_dates <- holiday_dates %>% 
-  filter(Date > as.Date("2014-12-26", tz = "UTC"))
+flex <-  holidays %>% filter(Fixed == FALSE & Global == TRUE & Name != "Assumption")
+#It is easy to handle Flex
+flex <- flex %>%filter(Date < "2024-07-01")
+flex <- flex%>%filter(Date > "2021-01-10")
 
-holiday_dates <- holiday_dates %>%
-  filter(Date < as.Date("2024-08-01", tz = "UTC"))
+location <- function(data,holidays){
+  locs <- c()
+  for (i in 1:length(holidays$Date)) {
+    locs[i] <- which(data$DateTime == holidays$Date[i])
+  }
+  return(locs)
+}
+locs_flex <- location(all_DF,flex)
+zzz <- function(t){
+plot(all_DF$DateTime[(t-24):(t+23+24)],all_DF$Load_ACT[(t-24):(t+23+24)],type="l",ylim=c(40000,75000))
+lines(all_DF$DateTime[(t-24):(t+23+24)],all_DF$Load_ACT[(t-7*24-24):(t-7*24+23+24)],col="red")
+lines(all_DF$DateTime[(t-24):(t+23+24)],all_DF$Load_ACT[(t+7*24-24):(t+7*24+23+24)],col="blue")
+}
+zzz(locs_flex[3])
+
+flex_d <- rep(0,length(all_DF$DateTime))
+for (i in locs_flex) {
+  flex_d[(i-12):(i+12)] <- 1
+}
+####
+fixed <- fixed %>% filter(Date > "2020-01-01")%>%filter(Date < "2025-01-01")
+fixed %>%filter(Name == unique(fixed$Name)[9])
 
 
-holiday_dates <- holiday_dates %>% filter(CountryCode == "ES")
-unique(holiday_dates$Name)
+locs <- (fixed %>%filter(Name == unique(fixed$Name)[2]))
+locs2 <- location(all_DF,locs)
 
-#Index  no of public holidays at all regions 
-idx_h <- which(is.na(holiday_dates$Counties)== TRUE)
+zzz <- function(t){
+  plot(all_DF$DateTime[(t-24):(t+23+24)],all_DF$Load_ACT[(t-24):(t+23+24)],type="l",xlab=weekdays(all_DF$DateTime[t]),ylim=c(30000,55000))
+  lines(all_DF$DateTime[(t-24):(t+23+24)],all_DF$Load_ACT[(t-7*24-24):(t-7*24+23+24)],col="red")
+  lines(all_DF$DateTime[(t-24):(t+23+24)],all_DF$Load_ACT[(t+7*24-24):(t+7*24+23+24)],col="blue")
+}
+zzz(locs2[4])
 
-for (i in idx_h) {
-  holiday_dates$Counties[i] <- "All"
+
+####
+regional_fixed
+regional_flex
+
+
+
+  }
+  
+  basis <- coefficients
+  return(basis)
 }
 
-2015-02-28 00:00:00 Día de Andalucía                  Day of Andalucía    
+# Assume 'high_level_load_target' and 'actual_load_target' are vectors of hourly mean loads
+# Assume 'timestamp' is a vector of POSIXct timestamps
 
-2016-02-28
-which(dst_data$DateTime == as.Date("2015-02-28 00:00:00"))#1392'Saturday
-which(dst_data$DateTime == as.Date("2015-02-21 00:00:00"))#
+# Create the basis functions
+flex_basis <- create_flex_basis(load_data$timestamp, holidays)
+fix_basis <- create_fix_basis(load_data$timestamp, holidays, actual_load_target, high_level_load_target)
 
-which(dst_data$DateTime == as.Date("2016-02-28 00:00:00"))#10152#Sunday
-which(dst_data$DateTime == as.Date("2016-02-21 00:00:00"))#
+# Combine the basis functions into the load data
+load_data <- load_data %>%
+  mutate(flex_basis = flex_basis,
+         fix_basis = fix_basis)
 
-which(dst_data$DateTime == as.Date("2017-02-28 00:00:00"))#10152 Tuesday
-which(dst_data$DateTime == as.Date("2017-02-21 00:00:00"))# Tuesday
-
-
-
-plot(0:23,dst_data[1392:1415,3],type="l",ylim=c(18000,30000))
-lines(0:23,dst_data[10152:10175,3],col="red",type="l")
-lines(0:23,dst_data[18936:18959,3],col="blue",type="l")
-#A week befores
-lines(0:23,dst_data[1224:1247,3],col="green",type="l")
-lines(0:23,dst_data[9984:10007,3],col="orange",type="l")
-lines(0:23,dst_data[18768:18791,3],col="purple",type="l")
-legend("topright",                      # Position of the legend in the plot
-       legend = c("28FEB15 Public Holiday- Saturday",       
-                  "28FEB16 Public Holiday- Sunday",         
-                  "28FEB17 Public Holiday- Tuesday",        
-                  "A week before of 28FEB15- Saturday",
-                  "A week before of 28FEB16- Sunday",#
-                  "A week before of 28FEB17- Tuesday"),# 
-       col = c("black", "red", "blue", "green", "orange", "purple"),  
-       lty = 1,                          
-       cex = 0.8) 
 
