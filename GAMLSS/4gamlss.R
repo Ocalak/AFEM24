a10f4422155704dc49227d8d51eabc9e6963ab79
Ocalak <96614838+Ocalak@users.noreply.github.com>
2024-07-09 07:45:03 +0200

    Create 4gamlss.R

---
+++ GAMLSS/4gamlss.R
@@ -0,0 +1,50 @@
+gamlss_validation <- function(data,window_l,dist,){
+  #Valdiaiton is 365 days. I fit the model every 30 days 
+  
+for (t in inx) {
+  
+  Y <- model.matrix(~(Hod+Hoy+How+HolFix+HolFlx+XHol+Temp_Lag2+Temp_Lag4+Temp_Lag24+Temp_ACT+Temp_Lag1+
+                        Temp_Lag168 +Load_Lag168+ Load_Lag144+Load_Lag48+Load_Lag24+Temp_max+Temp_min),data=data[(window_l)+(t*24),])
+  
+  ultra1 <- data.frame("Load_DA"=data$Load_DA[(window_l)+(t*24)], Y[,-1])
+  
+  
+  
+  lasoo_no <- gamlss(Load_DA~gnet(x.vars=names(ultra1)[-c(1)],method = "IC", ICpen="BIC"),
+                     sigma.fo= ~gnet(x.vars=  names(ultra1)[-c(1)],method = "IC", ICpen="BIC"),
+                     data=ultra1, family=Dist,method = mixed(500,500),control=contr)
+  
+  
+  # Construct the formula string for mu
+  
+  mu_formula <- paste("Load_DA ~", paste(paste0("pb(", names(which(tail(getSmo(lasoo_no, "mu"), 1)[[1]]$beta != 0)), ")"), collapse = " + "))
+  
+  sigma_formula <- paste("~", paste(paste0("pb(", names(which(tail(getSmo(lasoo_no, "sigma"), 1)[[1]]$beta != 0)), ")"), collapse = " + "))
+  
+  mu_formula <- as.formula(mu_formula)
+  
+  sigma_formula <- as.formula(sigma_formula)
+  
+  
+  new_model <- gamlss(mu_formula,sigma.fo=sigma_formula,family=Dist, data=data[(window_l)+t*24),],method=mixed(50,50),control = gamlss.control(c.crit = 0.1))  
+  
+  #save(new_model,file="~/Desktop/AFEM24/Normaldistfit1.RData")
+  
+  if(new_model$converge == FALSE){
+    refit(new_model)
+  }
+  #idx <- length(unique(yday(data$DateTime[43089:51848])))# == "2023-12-01 00:00:00")
+  
+  for (i in 0:29) {
+    val_NO_mu1sd1[i*2+1,1:168] <-  predictAll(new_model,newdata=data[(43809+t*24+i*24):(43976+t*24+i*24),])$mu[1:168]%>%exp()
+    val_NO_mu1sd1[i*2+2,1:168] <-  predictAll(new_model,newdata=data[(43089+t*24+i*24):(43256+t*24+i*24),])$sigma[1:168]%>%exp()
+  }
+  val_NO_mu1sd1 <<- val_NO_mu1sd1 %>%as.data.frame()%>%t()
+  write_csv(val_NO_mu1sd1,file= paste0("~/Desktop/AFEM24/NO-VAL/NO_VAL_PRE",t,".csv"))
+}
+
+
+
+
+
+  
